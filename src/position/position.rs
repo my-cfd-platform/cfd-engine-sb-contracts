4f253b3e558010c0081ea6b0d543a7091a3d01bb
Hays R <wonderhays@gmail.com>
2022-10-28 11:30:06 +0300

    updates

--- src/position/position.rs
+++ src/position/position.rs
@@ -40,36 +40,38 @@ pub struct OrderSbModel{
     pub created: u64,
     #[prost(message, tag = "7")]
     pub open_price: Option<f64>,
-    #[prost(message, tag = "8")]
-    pub execute_date: Option<u64>,
-    #[prost(message, tag = "9")]
-    pub execute_price: Option<f64>,
-    #[prost(enumeration = "OrderSide", tag = "10")]
+    #[prost(enumeration = "OrderSide", tag = "8")]
     pub side: i32,
-    #[prost(message, tag = "11")]
+    #[prost(message, tag = "9")]
     pub tp_percent: Option<f64>,
-    #[prost(message, tag = "12")]
+    #[prost(message, tag = "10")]
     pub tp_rate: Option<f64>,
-    #[prost(message, tag = "13")]
+    #[prost(message, tag = "11")]
     pub sl_percent: Option<f64>,
-    #[prost(message, tag = "14")]
+    #[prost(message, tag = "12")]
     pub sl_rate: Option<f64>,
-    #[prost(uint64, tag = "15")]
+    #[prost(uint64, tag = "13")]
     pub last_update_date: u64,
-    #[prost(message, tag = "16")]
-    pub open_process_id: Option<String>,
-    #[prost(string, tag = "17")]
+    #[prost(message, tag = "14")]
+    pub create_process_id: Option<String>,
+    #[prost(string, tag = "15")]
     pub last_update_process_id: String,
-    #[prost(message, tag = "18")]
+    #[prost(message, tag = "16")]
     pub profit: Option<f64>,
-    #[prost(message, tag = "19")]
+    #[prost(message, tag = "17")]
     pub close_date: Option<u64>,
-    #[prost(message, tag = "20")]
+    #[prost(message, tag = "18")]
     pub close_reason: Option<i32>,
-    #[prost(message, tag = "21")]
+    #[prost(message, tag = "19")]
     pub close_price: Option<f64>,
-    #[prost(message, repeated, tag = "22")]
-    pub metadata: Vec<OrderMetadata>
+    #[prost(message, repeated, tag = "20")]
+    pub metadata: Vec<OrderMetadata>,
+    #[prost(message, tag = "21")]
+    pub cancel_order_date: Option<u64>,
+    #[prost(message, tag = "22")]
+    pub cancel_order_process_id: Option<String>,
+    #[prost(message, tag = "23")]
+    pub close_process_id: Option<String>,
 }
 
 #[derive(PartialEq, ::prost::Message)]
@@ -83,5 +85,5 @@ pub struct PositionPersistenceEvent{
     #[prost(message, tag = "4")]
     pub close_position: Option<OrderSbModel>,
     #[prost(message, tag = "5")]
-    pub open_position: Option<OrderSbModel>
+    pub create_position: Option<OrderSbModel>
 }
