73b18a24476c3173060e180bec7ce55b22d61233
HaysR <57479934+RomanSavh@users.noreply.github.com>
2023-03-13 16:17:22 +0200

    Update bidask.rs

--- src/prices/bidask.rs
+++ src/prices/bidask.rs
@@ -1,12 +1,12 @@
-#[derive(PartialEq, ::prost::Message)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 #[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "bidask")]
-pub struct BidAsk {
+pub struct BidAskSbModel {
     #[prost(string, tag = "1")]
-    pub instrument_id: String,
-    #[prost(double, tag = "2")]
-    pub bid: f64,
+    pub id: ::prost::alloc::string::String,
+    #[prost(uint64, tag = "2")]
+    pub date_time_unix_milis: u64,
     #[prost(double, tag = "3")]
+    pub bid: f64,
+    #[prost(double, tag = "4")]
     pub ask: f64,
-    #[prost(uint64, tag = "4")]
-    pub date_time: u64,
 }
