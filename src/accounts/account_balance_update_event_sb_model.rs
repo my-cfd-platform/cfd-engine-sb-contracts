a1c6de63105597f8c6747bf9bcf3c6f6f07b5f2b
RsSvc <wonderhays@gmail.com>
2023-03-16 19:42:05 +0200

    model extend

--- src/accounts/account_balance_update_event_sb_model.rs
+++ src/accounts/account_balance_update_event_sb_model.rs
@@ -15,8 +15,8 @@ pub struct AccountBalanceUpdateOperationSbModel {
     pub id: String,
     #[prost(string, tag = "2")]
     pub trader_id: String,
-    #[prost(uint64, tag = "3")]
-    pub account_id: u64,
+    #[prost(string, tag = "3")]
+    pub account_id: String,
     #[prost(enumeration = "AccountBalanceUpdateOperationType", tag = "4")]
     pub operation_type: i32,
     #[prost(message, tag = "5")]
@@ -25,12 +25,17 @@ pub struct AccountBalanceUpdateOperationSbModel {
     pub delta: f64,
     #[prost(uint64, tag = "7")]
     pub date_time_unix_ms: u64,
+    #[prost(string, tag = "8")]
+    pub comment: String,
+    #[prost(message, tag = "9")]
+    pub reference_operation_id: Option<String>,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum AccountBalanceUpdateOperationType {
-    BalanceCorrection = 0,
-    Withdrawal = 1,
-    Deposit = 2,
+    Trading = 0,
+    BalanceCorrection = 1,
+    Withdrawal = 2,
+    Deposit = 3,
 }
