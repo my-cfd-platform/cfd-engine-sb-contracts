86d1cadb28764d468bc53924521666a4d9180c47
RsSvc <wonderhays@gmail.com>
2023-05-15 15:47:21 +0300

    added base/quote into bid ask

--- src/prices/bidask.rs
+++ src/prices/bidask.rs
@@ -9,4 +9,8 @@ pub struct BidAskSbModel {
     pub bid: f64,
     #[prost(double, tag = "4")]
     pub ask: f64,
+    #[prost(message, tag = "5")]
+    pub base: Option<String>,
+    #[prost(message, tag = "6")]
+    pub quote: Option<String>,
 }
