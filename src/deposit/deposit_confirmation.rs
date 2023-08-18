e79bcc22db8a92437b65d4ecec49c9d09f55dbbc
amigin <amigin@gmail.com>
2023-08-18 11:35:15 +0300

    Added PaymentSystemId

--- src/deposit/deposit_confirmation.rs
+++ src/deposit/deposit_confirmation.rs
@@ -2,7 +2,9 @@
 #[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "deposit-confirmation")]
 pub struct DepositConfirmationSbModel {
     #[prost(string, tag = "1")]
-    pub reference_id: String,
+    pub payment_system_id: String,
     #[prost(string, tag = "2")]
+    pub reference_id: String,
+    #[prost(string, tag = "3")]
     pub tech_data: String,
 }
