76a256f9c1751ff6fdd00b83cbfe63970e897779
HaysR <57479934+RomanSavh@users.noreply.github.com>
2024-03-19 16:40:20 +0200

    fixed positions import

--- src/position/mod.rs
+++ src/position/mod.rs
@@ -2,10 +2,8 @@ mod position;
 mod pending_position;
 mod margin_call_hit;
 mod topping_up;
-mod position_margin_call;
 
 pub use position::*;
 pub use pending_position::*;
 pub use margin_call_hit::*;
 pub use topping_up::*;
-pub use margin_call_hit::*;
